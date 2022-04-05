96f6097a48a0ea92b431123351fbe8e9be43f43c
amantiwari1114 <73477100+amantiwari1114@users.noreply.github.com>
2022-04-05 22:49:41 +0530

    Add files via upload

---
+++ resources/views/index.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                Life only comes around once, so do whatever makes you happy, and be with whoever makes you smile.
+                </h1>
+                <a 
+                    href="/blog"
+                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="https://media.istockphoto.com/photos/blog-and-information-website-concept-workplace-background-with-text-picture-id1130150680?k=20&m=1130150680&s=612x612&w=0&h=xS0ephM0yuIC3ploNT4CbbOoZmZ8fxQNJlrB4yPJC9k=" width="700" alt="">
+        </div>
+
+        <div class="m-auto sm:m-auto text-left w-4/5 block">
+            <h2 class="text-3xl font-extrabold text-gray-600">
+                Struggling to be a better web developer?
+            </h2>
+            
+            <p class="py-8 text-gray-500 text-s">
+            To become a Web Developer, you should have an understanding of HTML, CSS, and JavaScript. It's also recommended to learn about CSS and CSS frameworks. Developing these fundamental web development skills will give you the foundation and logic for communicating with programming languages.
+            </p>
+
+            <p class="font-extrabold text-gray-600 text-s pb-9">
+            While learning the foundational skills to become a Web Developer is relatively straightforward, becoming a good Web Developer can be more challenging, requiring ongoing learning and effort over years.
+            </p>
+
+            <a 
+                href="/blog"
+                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+                Find Out More
+            </a>
+        </div>
+    </div>
+
+    <div class="text-center p-15 bg-black text-white">
+        <h2 class="text-2xl pb-5 text-l"> 
+            I'm an expert in...
+        </h2>
+
+        <span class="font-extrabold block text-4xl py-1">
+            Python , C++
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            HTML
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            CSS
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            JavaScript
+        </span>
+    </div>
+
+    <div class="text-center py-15">
+        <span class="uppercase text-s text-gray-400">
+            Blog
+        </span>
+
+        <h2 class="text-4xl font-bold py-10">
+            Recent Posts
+        </h2>
+
+        <p class="m-auto w-4/5 text-gray-500">
+            LARAVEL
+        </p>
+    </div>
+
+    <div class="sm:grid grid-cols-2 w-4/5 m-auto">
+        <div class="flex bg-yellow-700 text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+                <span class="uppercase text-xs">
+                    LARAVEL
+                </span>
+
+                <h3 class="text-xl font-bold py-10">
+                Laravel is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern and based on Symfony.
+                </h3>
+
+                <a 
+                    href=""
+                    class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                    Find Out More
+                </a>
+            </div>
+        </div>
+        <div >
+            <img src="https://cdn.educba.com/academy/wp-content/uploads/2019/09/What-is-Laravel.png" alt="">
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,93 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Blog
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Login
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Register
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Find Us
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                       Aman Tiwari
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Lovely Professional University
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        9876543210
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        aman.11909439@lpu.in
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Latest posts
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Why we love tech
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Why we love design
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Why to use Laravel
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Why PHP is the best
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright 2017-2021 Code With Dary. All Rights Reserved
+    </p>
+</footer>
